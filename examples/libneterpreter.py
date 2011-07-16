d173c3ed2daaa05c210b1c4074fc0576525a5c05
Nadeem Douba <ndouba@gmail.com>
2011-07-15 20:08:11 -0700

    added copyright notices

--- examples/libneterpreter.py
+++ examples/libneterpreter.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
 
+# libneterpreter - interactive libnet interpreter
+# Copyright (C) 2011 Nadeem Douba
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Nadeem Douba <ndouba at gmail dot com>
+# 348 Patricia Ave, Ottawa, ON, K1Z 6G6
+# Canada
+
 import getopt
 
 import sys
